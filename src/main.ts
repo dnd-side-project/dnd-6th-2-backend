fd464fff5c496782481fbcc77b7b8e9945183538
ohyujeong <ohlife4@gmail.com>
2022-01-23 15:40:10 +0900

    Swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('DND2 API')
+  .setDescription('DND 2조의 API 문서입니다.')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
+
+
 }
 bootstrap();
