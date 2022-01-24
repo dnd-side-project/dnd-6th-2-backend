b20f3d5d0ebd9dc6397afb44d5f83e9843bfa157
yesjjin99 <yesjjin99@naver.com>
2022-01-25 02:11:44 +0900

    Revert "Fix ESLint settings"
    This reverts commit adc574da970f4ee912f2334cd080d68dba32e4b6.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,11 +20,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
   },
 };
