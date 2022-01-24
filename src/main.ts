e23f30141458b5bfe3210f2f8a146157d97e9457
yesjjin99 <yesjjin99@naver.com>
2022-01-25 02:34:03 +0900

    Fix ESLint settings
    vscode에서 발생하는 ESLint(Prettier) 에러 해결

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('DND2 API')
-  .setDescription('DND 2조의 API 문서입니다.')
-  .setVersion('1.0')
-  .build();
+    .setTitle('DND2 API')
+    .setDescription('DND 2조의 API 문서입니다.')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(3000);
-
-
 }
 bootstrap();
