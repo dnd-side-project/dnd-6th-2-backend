b17b56aa037c034aa9ac6742311177e7ef405c01
yesjjin99 <yesjjin99@naver.com>
2022-02-08 04:02:16 +0900

    Add send email(find password API) codes
    이메일 인증 관련 코드 작성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './passport/auth.strategy';
@@ -11,6 +13,25 @@ import { User, UserSchema } from './schemas/user.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_ID,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      template: {
+        dir: process.cwd() + '/template/',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     JwtModule.register({
       secret: process.env.SECRET_KEY,
       signOptions: {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AuthCredentialDto } from './dto/auth.dto';
 import { AuthRepository } from './repository/auth.repository';
 import { User } from './schemas/user.schema';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
-  constructor(private authRepository: AuthRepository) {}
+  constructor(
+    private authRepository: AuthRepository,
+    private mailerService: MailerService,
+  ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<User> {
     return this.authRepository.signUp(authCredentialDto);
@@ -15,5 +19,24 @@ export class AuthService {
     authCredentialDto: AuthCredentialDto,
   ): Promise<{ accessToken: string }> {
     return this.authRepository.logIn(authCredentialDto);
+  }
+
+  async sendEmail(email: string): Promise<string> {
+    try {
+      const authCode: string = Math.random().toString().substring(2, 7); // 6글자
+
+      const mailOptions = {
+        from: process.env.EMAIL_ID,
+        to: email,
+        subject: '이메일 인증',
+        text: '이메일 인증 코드 ' + authCode,
+      };
+
+      await this.mailerService.sendMail(mailOptions);
+
+      return authCode;
+    } catch (e) {
+      throw new InternalServerErrorException('이메일 발신 실패');
+    }
   }
 }
